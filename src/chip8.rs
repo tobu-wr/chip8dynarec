06a46956c1499174c64e905b674231dc34d881a6
rwilfried <wilfried.rabouin@gmail.com>
2017-09-30 23:38:53 +0200

    Fix rom loading

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,15 +1,31 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Default)]
+const MEMORY_SIZE: usize = 0x1000;
+
 pub struct Chip8 {
-	rom: Vec<u8>
+	memory: [u8; MEMORY_SIZE]
 }
 
 impl Chip8 {
+	pub fn new() -> Chip8 {
+		Chip8 {
+			memory: [0; MEMORY_SIZE]
+		}
+	}
+
 	pub fn load_rom(&mut self, filename: String) {
+		// read file
 		let mut file = File::open(filename).expect("file not found");
-		file.read_to_end(&mut self.rom).expect("something went wrong reading the file");
+		let mut buffer: Vec<u8> = Vec::new();
+		file.read_to_end(&mut buffer).expect("something went wrong reading the file");
+
+		// copy to memory
+		let mut i = 0x200;
+		for byte in buffer {
+			self.memory[i] = byte;
+			i += 1;
+		}
 	}
 
 	pub fn run(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 mod chip8;
 
-use std::env;
-
 fn main() {
-	let filename = env::args().nth(1).unwrap();
-	let mut chip8 = chip8::Chip8::default();
+	let filename = std::env::args().nth(1).unwrap();
+	let mut chip8 = chip8::Chip8::new();
 	chip8.load_rom(filename);
 	chip8.run();
 }
