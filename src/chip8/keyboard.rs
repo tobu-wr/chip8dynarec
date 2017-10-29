0e30ff1e27923e2f9bce51d211da63b802c4867b
rwilfried <wilfried.rabouin@gmail.com>
2017-10-29 17:40:07 +0100

    Change keys

--- src/chip8/keyboard.rs
+++ src/chip8/keyboard.rs
@@ -21,32 +21,32 @@ impl Keyboard {
 	pub fn update_key_states(&mut self) {
 		for event in self.events.poll_iter() {
 			match event {
-				Event::KeyDown { keycode: Some(Keycode::Num0), ..} => self.key_states[0x0] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num1), ..} => self.key_states[0x1] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num2), ..} => self.key_states[0x2] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num3), ..} => self.key_states[0x3] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num4), ..} => self.key_states[0x4] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num5), ..} => self.key_states[0x5] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num6), ..} => self.key_states[0x6] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num7), ..} => self.key_states[0x7] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num8), ..} => self.key_states[0x8] = true,
-				Event::KeyDown { keycode: Some(Keycode::Num9), ..} => self.key_states[0x9] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp0), ..} => self.key_states[0x0] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp1), ..} => self.key_states[0x1] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp2), ..} => self.key_states[0x2] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp3), ..} => self.key_states[0x3] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp4), ..} => self.key_states[0x4] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp5), ..} => self.key_states[0x5] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp6), ..} => self.key_states[0x6] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp7), ..} => self.key_states[0x7] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp8), ..} => self.key_states[0x8] = true,
+				Event::KeyDown { keycode: Some(Keycode::Kp9), ..} => self.key_states[0x9] = true,
 				Event::KeyDown { keycode: Some(Keycode::A), ..} => self.key_states[0xA] = true,
 				Event::KeyDown { keycode: Some(Keycode::B), ..} => self.key_states[0xB] = true,
 				Event::KeyDown { keycode: Some(Keycode::C), ..} => self.key_states[0xC] = true,
 				Event::KeyDown { keycode: Some(Keycode::D), ..} => self.key_states[0xD] = true,
 				Event::KeyDown { keycode: Some(Keycode::E), ..} => self.key_states[0xE] = true,
 				Event::KeyDown { keycode: Some(Keycode::F), ..} => self.key_states[0xF] = true,
-				Event::KeyUp { keycode: Some(Keycode::Num0), ..} => self.key_states[0x0] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num1), ..} => self.key_states[0x1] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num2), ..} => self.key_states[0x2] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num3), ..} => self.key_states[0x3] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num4), ..} => self.key_states[0x4] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num5), ..} => self.key_states[0x5] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num6), ..} => self.key_states[0x6] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num7), ..} => self.key_states[0x7] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num8), ..} => self.key_states[0x8] = false,
-				Event::KeyUp { keycode: Some(Keycode::Num9), ..} => self.key_states[0x9] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp0), ..} => self.key_states[0x0] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp1), ..} => self.key_states[0x1] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp2), ..} => self.key_states[0x2] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp3), ..} => self.key_states[0x3] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp4), ..} => self.key_states[0x4] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp5), ..} => self.key_states[0x5] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp6), ..} => self.key_states[0x6] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp7), ..} => self.key_states[0x7] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp8), ..} => self.key_states[0x8] = false,
+				Event::KeyUp { keycode: Some(Keycode::Kp9), ..} => self.key_states[0x9] = false,
 				Event::KeyUp { keycode: Some(Keycode::A), ..} => self.key_states[0xA] = false,
 				Event::KeyUp { keycode: Some(Keycode::B), ..} => self.key_states[0xB] = false,
 				Event::KeyUp { keycode: Some(Keycode::C), ..} => self.key_states[0xC] = false,
@@ -66,43 +66,43 @@ impl Keyboard {
 		loop {
 			for event in self.events.wait_iter() {
 				match event {
-					Event::KeyDown { keycode: Some(Keycode::Num0), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp0), ..} => {
 						self.key_states[0x0] = true;
 						return 0x0
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num1), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp1), ..} => {
 						self.key_states[0x1] = true;
 						return 0x1
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num2), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp2), ..} => {
 						self.key_states[0x2] = true;
 						return 0x2
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num3), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp3), ..} => {
 						self.key_states[0x3] = true;
 						return 0x3
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num4), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp4), ..} => {
 						self.key_states[0x4] = true;
 						return 0x4
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num5), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp5), ..} => {
 						self.key_states[0x5] = true;
 						return 0x5
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num6), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp6), ..} => {
 						self.key_states[0x6] = true;
 						return 0x6
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num7), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp7), ..} => {
 						self.key_states[0x7] = true;
 						return 0x7
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num8), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp8), ..} => {
 						self.key_states[0x8] = true;
 						return 0x8
 					},
-					Event::KeyDown { keycode: Some(Keycode::Num9), ..} => {
+					Event::KeyDown { keycode: Some(Keycode::Kp9), ..} => {
 						self.key_states[0x9] = true;
 						return 0x9
 					},
@@ -130,16 +130,16 @@ impl Keyboard {
 						self.key_states[0xF] = true;
 						return 0xF
 					},
-					Event::KeyUp { keycode: Some(Keycode::Num0), ..} => self.key_states[0x0] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num1), ..} => self.key_states[0x1] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num2), ..} => self.key_states[0x2] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num3), ..} => self.key_states[0x3] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num4), ..} => self.key_states[0x4] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num5), ..} => self.key_states[0x5] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num6), ..} => self.key_states[0x6] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num7), ..} => self.key_states[0x7] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num8), ..} => self.key_states[0x8] = false,
-					Event::KeyUp { keycode: Some(Keycode::Num9), ..} => self.key_states[0x9] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp0), ..} => self.key_states[0x0] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp1), ..} => self.key_states[0x1] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp2), ..} => self.key_states[0x2] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp3), ..} => self.key_states[0x3] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp4), ..} => self.key_states[0x4] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp5), ..} => self.key_states[0x5] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp6), ..} => self.key_states[0x6] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp7), ..} => self.key_states[0x7] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp8), ..} => self.key_states[0x8] = false,
+					Event::KeyUp { keycode: Some(Keycode::Kp9), ..} => self.key_states[0x9] = false,
 					Event::KeyUp { keycode: Some(Keycode::A), ..} => self.key_states[0xA] = false,
 					Event::KeyUp { keycode: Some(Keycode::B), ..} => self.key_states[0xB] = false,
 					Event::KeyUp { keycode: Some(Keycode::C), ..} => self.key_states[0xC] = false,
