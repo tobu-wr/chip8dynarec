501f02ab732e2bf1bca64a053da99447855385f8
rwilfried <wilfried.rabouin@gmail.com>
2017-11-01 11:21:57 +0100

    Fix segfault caused by vector reallocation

--- src/chip8/codecache.rs
+++ src/chip8/codecache.rs
@@ -1,29 +1,32 @@
+extern crate libc;
+
 use std::ops::Index;
-use std::ptr;
+use std::{ptr, mem};
 
 use chip8::MEMORY_SIZE;
 use chip8::codeblock::CodeBlock;
 
 pub struct CodeCache {
-	code_blocks: Vec<CodeBlock>,
-	map: [*const CodeBlock; MEMORY_SIZE]
+	code_blocks: [*mut CodeBlock; MEMORY_SIZE]
 }
 
 impl CodeCache {
 	pub fn new() -> CodeCache {
 		CodeCache {
-			code_blocks: Vec::new(),
-			map: [ptr::null(); MEMORY_SIZE]
+			code_blocks: [ptr::null_mut(); MEMORY_SIZE]
 		}
 	}
 
 	pub fn contains_address(&self, address: u16) -> bool {
-		!self.map[address as usize].is_null()
+		!self.code_blocks[address as usize].is_null()
 	}
 
 	pub fn insert(&mut self, address: u16, code_block: CodeBlock) {
-		self.code_blocks.push(code_block);
-		self.map[address as usize] = self.code_blocks.last().unwrap();
+		unsafe {
+			let new_ptr = libc::malloc(mem::size_of::<CodeBlock>()) as *mut CodeBlock;
+			ptr::write(new_ptr, code_block);
+			self.code_blocks[address as usize] = new_ptr;
+		}
 	}
 }
 
@@ -31,6 +34,6 @@ impl Index<u16> for CodeCache {
 	type Output = CodeBlock;
 
 	fn index(&self, address: u16) -> &CodeBlock {
-        unsafe { &*self.map[address as usize] }
+		unsafe { self.code_blocks[address as usize].as_ref().unwrap() }
     }
 }
